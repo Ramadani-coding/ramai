1f091a67805a21e0f85dab73d7a5d0f66512c808
Ramadani-coding <ramacoding9@gmail.com>
2024-05-22 17:46:33 +0800

    Fix: input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,38 +1,14 @@
-import { useState } from "react";
+import { Textarea } from "@chakra-ui/react";
 
 const Input = () => {
-  const [text, setText] = useState("");
-
-  const handleChange = (e) => {
-    let value = e.target.value;
-    // Insert newline character after 88 characters
-    if (value.length > 88) {
-      const lastSpaceIndex = value.lastIndexOf(" ", 88);
-      value =
-        value.substring(0, lastSpaceIndex) +
-        "\n" +
-        value.substring(lastSpaceIndex + 1);
-    }
-    setText(value);
-  };
-
   return (
     <div className="h-[80vh] flex justify-center items-end w-full">
-      <div className="relative w-full py-2 mx-5 md:py-2 md:w-1/2">
-        <textarea
-          className="bg-[#F4F4F4] text-lg py-3 px-5 md:px-10 rounded-lg md:rounded-md outline-none w-full text-[#000000] resize-none overflow-y-auto max-h-40"
-          id="content"
-          placeholder="Mau tau apa?"
-          value={text}
-          onChange={handleChange}
-          rows={1}
-          style={{ height: "auto" }}
-          onInput={(e) => {
-            e.target.style.height = "auto";
-            e.target.style.height = `${e.target.scrollHeight}px`;
-          }}
+      <div className="flex w-full gap-4 py-2 mx-5 md:py-2 md:w-1/2">
+        <Textarea
+          className="bg-[#F4F4F4] text-lg py-3 px-5 md:px-10 rounded-lg md:rounded-md outline-none w-full text-[#000000] resize-none overflow-y-auto"
+          placeholder="Mau cari apa?"
         />
-        <button className="absolute right-3 top-[45%] transform -translate-y-1/2 bg-[#D7D7D7] p-2 rounded-full hover:bg-black hover:text-white transition-all">
+        <button className=" bg-[#D7D7D7] flex justify-center items-center w-20 p-2 rounded-lg hover:bg-black hover:text-white transition-all">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
